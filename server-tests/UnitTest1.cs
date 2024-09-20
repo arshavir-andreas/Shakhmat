46d520edcffa9ee0da8bb118771da97669d1500a
arshavir-andreas <mandaniainaandriamanantena@gmail.com>
2024-09-20 17:55:40 +0300

    test: test the refactored Oracle DB connection files

--- server-tests/UnitTest1.cs
+++ server-tests/UnitTest1.cs
@@ -4,13 +4,37 @@ namespace Tests {
     public class UnitTest1 {
         [Fact]
         public async Task OracleDatabaseConnectionShouldBeSuccessful() {
-            string connectionString = "User Id=some_user; Password=password; Data Source=localhost:1521; ";
+            string connectionString = "User Id=shakhmat; Password=UkcREWFel0jWpbnA; Data Source=localhost:1521; ";
 
             using var conn = new OracleConnection(connectionString);
 
             await conn.OpenAsync();
 
             Assert.Equal(1, 1);
+        }
+
+        [Fact]
+        public async Task UsersTableShouldExist() {
+            string connectionString = "User Id=shakhmat; Password=UkcREWFel0jWpbnA; Data Source=localhost:1521; ";
+
+            using var conn = new OracleConnection(connectionString);
+
+            await conn.OpenAsync();
+
+            using var cmd = new OracleCommand(@"
+                SELECT COUNT(*) as user_count
+                FROM user_tables
+            ", conn);
+
+            using var reader = await cmd.ExecuteReaderAsync();
+
+            if (await reader.ReadAsync()) {
+                Assert.Equal(1, (decimal)reader["user_count"]);
+
+                return;
+            }
+
+            Assert.Fail("");
         }
     }
 }
